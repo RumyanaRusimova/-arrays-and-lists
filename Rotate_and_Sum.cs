215c3e515178fdcb1b81d8395b1fbf2f8e31cea5
RumyanaRusimova <rumyanarusimova@abv.bg>
2017-06-09 21:54:55 +0300

    Create Rotate_and_Sum.cs

---
+++ Rotate_and_Sum.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Rotate_and_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var rotationsCount = int.Parse(Console.ReadLine());
+            int[] sumArray = new int[arr.Length];
+
+
+            for (int i = 0; i < rotationsCount; i++)
+            {
+                RotateArray(arr);
+
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    sumArray[j] += arr[j];
+                 }
+            }
+            Console.WriteLine(string.Join(" ", sumArray));
+        }
+
+        private static void RotateArray(int[] arr)
+        {
+            var last = arr [arr.Length - 1];
+           
+            for (int i = arr.Length - 1; i > 0; i--)
+            {
+                arr[i] = arr[i - 1];
+            }
+            arr[0] = last;
+        }
+    }
+}
